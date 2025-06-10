61db783955693d3d8a77c52d37460da4324cc798
Samay Satardekar <mayursatardekar10@gmail.com>
2025-06-10 18:58:26 +0530

    Guessing game add Number input

--- main.rs
+++ main.rs
@@ -6,12 +6,13 @@ fn main() {
     println!("Please enter your guess");
 
     let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
     io::stdin()
         .read_line(&mut guess)
         .expect("Fail to read the input");
-
+    // converting the input to integer
+    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
     println!("You have entered your guess as: {guess}");
 }
