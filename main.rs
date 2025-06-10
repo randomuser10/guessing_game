2d97117ae494b23b09e1f311467d7e3e311692e1
Samay Satardekar <mayursatardekar10@gmail.com>
2025-06-10 18:07:38 +0530

    Guessing game updated

--- main.rs
+++ main.rs
@@ -1,13 +1,17 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Welcome to guessing game!");
-    println!("Please enter your guess.");
+    println!("Welcome to the Guessing Game!!");
+    println!("Please enter your guess");
 
     let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    println!("The secret number is: {secret_number}");
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read input");
-    println!("You guessed: {guess}");
+        .expect("Fail to read the input");
+
+    println!("You have entered your guess as: {guess}");
 }
